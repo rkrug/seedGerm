13361b5d90aa41866db4e17e54e7ae411b2a6ca7
Rainer M. Krug <R.M.Krug@gmail.com>
2013-05-17 19:06:00 +0200

    Initial commit
    Seed germination routines

---
+++ R/seedGermBasic.R
@@ -0,0 +1,130 @@
+## [[file:~/Documents/Projects/AlienManagementDrakensberg/sim/packages/seedGerm/seedGerm.org::*seedProdBasic][seedProdBasic:1]]
+##' Basic and deterministic germination based on seed bank
+##'
+##' Calculates number of seedlings and their density class based on probability that
+##' seeds in seed bank germinate and a function 
+##' @title 
+##' @param seedsSoilLayer name of layer contraining number of seeds in soil seed bank
+##' @param disturbancesLayer name of disturbance layer for species
+##' @param suitabilityLayer name of suitability layer for species
+##' @param pGermination proportion of seeds which will try to germinate, i.e. either germinate or are removed from soil seed bank in the process
+##' @param pGerminationDep function to determine actual probability of germination in dependanc of suitability and disturbance in cell
+##' @param seedlToDcCuts cuts for conversion of number of germinated seedlings to density classes (parameter$res$cellSize * species$WfWdc$seedling / 10000)
+##' @param dcClasses name of density classes (pecies$WfWdc$dc[length(species$WfWdc$dc):1])
+##' @param pGermLayer output: name of layer containing actual probabilities of germination per cell
+##' @param noSeedlingsLayer output: name of number of seedlings layer
+##' @param dcSeedlingsLayer output: name of seedlings density class (dc)
+##' @return 
+##' @author Dr Rainer M Krug
+germBasic <- function(
+  ## parameter, species) {
+  seedsSoilLayer,                       # layerName(species, type="seedsSoil", year=parameter$year)
+  disturbancesLayer,                    # layerName(species, type="disturbances", year=parameter$year)
+  suitabilityLayer,                     # suitName(species)
+  pGermination,                         # species$pGermination
+  pGerminationDep,                      # species$pGerminationDep
+
+  seedlToDcCuts,                        # parameter$res$cellSize * species$WfWdc$seedling / 10000
+  dcClasses,                            # species$WfWdc$dc[length(species$WfWdc$dc):1]
+
+  pGermLayer,                           # layerName(species, type="pGerm", parameter$year)
+  noSeedlingsLayer,                     # layerName(species, type="noSeedlings", parameter$year)
+  dcSeedlingsLayer                      # layerName(species, type="dcSeedlings", parameter$year) 
+  ) {
+  noS <- noSeedlingsLayer
+  pGerm <- pGermLayer 
+  
+  germ <- readRAST6(
+    c(
+      sS <- seedsSoilLayer,
+      dist <- disturbanceLayer,
+      suit <- suitabilityLayer
+      ),
+    NODATA=-1
+    )
+  
+  ## calculate number of seeds which will germinate
+  seedsGerm <- germ[[sS]] * pGermination
+  
+  ## adjust soil seed bank
+  germ[[sS]] <- germ[[sS]] - seedsGerm
+  
+  ## successfull germination probability is pGerminationDep for the species
+  germ[[4]] <- pGerminationDep(germ[[suit]], germ[[dist]])
+  mode(germ[[4]]) <- "double"
+  ## germ@proj4string <- parameter$proj4string
+  writeRAST6(
+    germ,
+    vname = pGermLayer,
+    zcol = 4,
+    NODATA = -1
+    )
+  
+  ## ## seeds <- readRAST6( "tempGerm", NODATA=-1, ignore.stderr=!options("asmDebug")[[1]] )
+  ## seeds <- tmpGerm ##sgdfToMatrix(seeds, 1)
+  
+  ## initialise seedl with -999
+  seedl <- rep(-999, length(seedsGerm))
+  
+  ## set seedl to NA for all cells which have pGerm <= 0 or seeds <= 0
+  seedl[pGerm <= 0] <- NA
+  seedl[seedsGerm <= 0] <- NA
+
+  ## set germ to NA for all cells which have is.na(pGerm) or is.na(seedsGerm)
+  seedl[is.na(seedsGerm)] <- NA
+  seedl[is.na(pGerm)] <- NA
+  
+  ## set seedl to seedsGerm for all cells which have pGerm == 1
+  rb <- (pGerm == 1)
+  rb[is.na(rb)] <- FALSE
+  seedl[rb] <- seedsGerm[rb]  
+  
+  ## identify all remaining cells (i.e. seedl == -999) cells with suitabilities 0 < suit < 1 and calculate recruits in these cells
+  rb <- (seedl == -999)
+  rb[is.na(rb)] <- FALSE
+  oldWarn <- options()$warn
+  options(warn=-1)
+  seedl[rb] <- rbinom(
+    sum(rb),
+    size=seeds[rb],
+    prob=pGerm[rb]
+    )
+  options(warn=oldWarn)
+  
+  seedl[seedl == 0] <- NA
+  germ[[5]] <- seedl  
+  mode(germ[[5]]) <- "double"
+  ## germ@proj4string <- parameter$proj4string
+  writeRAST6(
+    germ,
+    vname = noS,
+    zcol = 5,
+    NODATA = -1
+    )
+  
+  ## Reclassification of noSeedlings into dcSeedlings
+  dcSeedl <- as.numeric(
+    as.character(
+      cut(
+        x = seedl,
+        breaks = c(Inf, seedlToDcCuts),
+        labels = dcClasses,
+        include.lowest = TRUE
+        )
+      )
+    )
+  dcSeedl[dcSeedl == 0] <- NA
+  germ[[5]] <- dcSeedl  
+  mode(germ[[5]]) <- "double"
+  ## germ@proj4string <- parameter$proj4string
+  writeRAST6(
+    germ,
+    vname = dcSeedlingsLayer,
+    zcol = 5,
+    NODATA = -1
+    )
+}
+## seedProdBasic:1 ends here
+## [[file:~/Documents/Projects/AlienManagementDrakensberg/sim/packages/seedGerm/seedGerm.org::*seedGermBasic][seedGermBasic:1]]
+
+## seedGermBasic:1 ends here
---
+++ seedGerm-package.R
@@ -0,0 +1,14 @@
+##' Routines for spatial seedGerm for alien clearing
+##' 
+##' \tabular{ll}{
+##' Package: \tab seedGerm\cr
+##' Type: \tab Package\cr
+##' Version: \tab 0.0.1\cr
+##' Date: \tab 2013-05-16_15-09\cr
+##' License: \tab GPL (>= 2)\cr
+##' LazyLoad: \tab yes\cr
+##' }
+##'
+##' @docType package
+##' @author Rainer M Krug \email{Rainer@@krugs.de}
+NULL
