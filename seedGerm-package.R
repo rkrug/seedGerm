1ca38eb2ee1e447f8ac85be1e8ece94e743b12e2
Rainer M. Krug <R.M.Krug@gmail.com>
2013-06-04 16:48:13 +0200

    fixed minor bugs and checked - working

--- seedGerm-package.R
+++ seedGerm-package.R
@@ -4,7 +4,7 @@
 ##' Package: \tab seedGerm\cr
 ##' Type: \tab Package\cr
 ##' Version: \tab 0.0.1\cr
-##' Date: \tab 2013-05-16_15-09\cr
+##' Date: \tab 2013-06-04_15-35\cr
 ##' License: \tab GPL (>= 2)\cr
 ##' LazyLoad: \tab yes\cr
 ##' }
